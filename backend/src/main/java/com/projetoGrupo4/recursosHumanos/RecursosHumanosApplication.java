0a3c6967e0058dc7d28025a7a86134ff22e6d4a7
Emerson Goncalvez Luiz <emerson_egl@hotmail.com>
2023-06-26 15:43:54 -0300

    Projeto criado

---
+++ backend/src/main/java/com/projetoGrupo4/recursosHumanos/RecursosHumanosApplication.java
@@ -0,0 +1,13 @@
+package com.projetoGrupo4.recursosHumanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursosHumanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecursosHumanosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetoGrupo4/recursosHumanos/RecursosHumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoGrupo4.recursosHumanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursosHumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
